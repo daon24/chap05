b16e251c1992dfde530fe4a1c8abc0ac82b52648
daon24 <adfs80342@gmail.com>
2025-06-23 11:26:19 +0900

    배열 연습문제 공부

--- src/ex03.java
+++ src/ex03.java
@@ -11,13 +11,14 @@ public class ex03 {
         for(int i =0; i< arr.length; i++){
             System.out.print(arr[i]+" ");
         }
-
+        System.out.println();
         //배열 복사하기 위한 tmp 생성
         int[] tmp= new int[arr.length*2];
 
         //배열 복사하기
-        for(int i =0; i <arr.length; i++)
+        for(int i =0; i <arr.length; i++) {
             tmp[i] = arr[i];
+        }
         arr = tmp;
         System.out.println("변경 후");
         System.out.println(arr.length);
---
+++ src/ex04.java
@@ -0,0 +1,11 @@
+public class ex04 {
+    public static void main(String[] args) {
+    char[] abc = {'A','B','C','D'};
+    char[] num = {'0', '1', '2', '3','4','5','6','7','8','9'};
+        System.out.println(abc);
+        System.out.println(num);
+
+        char[] result = new char[abc.length + num.length];
+        System.arraycopy
+    }
+}
